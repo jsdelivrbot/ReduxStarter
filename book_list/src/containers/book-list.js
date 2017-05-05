dd4741d7574171ff767a42f0cf73697c930738c1
maxvanbel <maxvanbel@gmail.com>
2017-05-04 21:46:20 -0500

    booklist in container

--- book_list/src/containers/book-list.js
+++ book_list/src/containers/book-list.js
@@ -4,15 +4,15 @@ export default class Booklist extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-          <li key={book.title} className='list-group-item'>{book.list}</li>
-        );
-    });
+          <li key={book.title} className="list-group-item">{book.title}</li>
+        )
+    })
   }
   render () {
     return (
-      <ul className='list-group col-sm-4'>
-        {this.renderList()}
-      </ul>
-    );
+    <ul className="list-group col-sm-4">
+      {this.rendList()}
+    </ul>
+    )
   }
 }
