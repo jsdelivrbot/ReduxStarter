c6291fbf12107336affc48a75c0debdc5ad4e872
maxvanbel <maxvanbel@gmail.com>
2017-05-06 10:49:31 -0500

    add book detail container and pages

--- book_list/src/containers/book-detail.js
+++ book_list/src/containers/book-detail.js
@@ -3,15 +3,15 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component {
   render() {
+    // when the state hasn't booted up yet
     if (!this.props.book) {
-      return <div>Select a book to get started.</div>;
+      return <p>"Select a book to get started."</p>
     }
-
     return (
       <div>
         <h3>Details for:</h3>
         <div>Title: {this.props.book.title}</div>
-        <div>Pages: {this.props.book.pages}</div>
+        <div>Pages: {this.props.book.pages} </div>
       </div>
     );
   }
--- book_list/src/reducers/reducer_active_book.js
+++ book_list/src/reducers/reducer_active_book.js
@@ -1,10 +1,13 @@
-// State argument is not application state, only the state
+// State argument is not the application state, only the state
 // this reducer is responsible for
 export default function(state = null, action) {
+  // if the action has the action type of BOOK_SELECTED,
+  // then proceed with payload
   switch(action.type) {
-  case 'BOOK_SELECTED':
+  case 'BOOK_SELECTED' :
     return action.payload;
   }
 
+  // if anything else, then just return state
   return state;
 }
